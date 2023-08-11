08df1c05348e3ba2e0b7ea7a6a212ca31ab000d8
Emin Kumru <eminkumru135@outlook.com>
2023-08-11 12:22:23 +0300

    components

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,6 +25,19 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <div className="bg-gray-500">
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+        <div className="h-96"></div>
+      </div>
     </>
   );
 }
